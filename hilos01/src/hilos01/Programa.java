6b8bd7115632cd5151e3015a0609ba947c869805
CristinaL <200101@iessierradeguara.com>
2021-11-09 12:30:15 +0100

    hilos

---
+++ hilos01/src/hilos01/Programa.java
@@ -0,0 +1,21 @@
+package hilos01;
+
+public class Programa {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Soy el hilo principal, voy a lanzar la tarea");
+		//A través de la superclase
+		Tarea t1 = new Tarea();
+		t1.start(); //lanzamos el método start aunque hayamos implementado run
+		
+		//A través de la interfaz necesitamos crear un thread
+		Thread t2 = new Thread(new TareaRunnable());
+		t2.start();
+		
+		t2.join();
+		t1.join();
+		System.out.println("Este es el final del hilo principal");
+		
+	}
+
+}
---
+++ hilos01/src/hilos01/TareaRunnable.java
@@ -0,0 +1,18 @@
+package hilos01;
+
+public class TareaRunnable implements Runnable {
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 15; i++) {
+		      System.out.println("Soy un hilo runnable y esto es lo que hago");
+		      try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
