787e080534195ae939e61e9d3801ce2f834c5c7b
CristinaL <200101@iessierradeguara.com>
2021-11-15 10:29:42 +0100

    hilos2

---
+++ hilos2-ContadorPalabras/src/ContadorPalabras.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ContadorPalabras {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		//Creamos fichero 'texto.txt' -> en temrinal: echo "hola clase ¿cómo estáis?" > texto.txt
+		String rutaFichero = "/home/alumno/texto.txt";
+		//Si se le pasa un fichero por argumento, leerá ese fichero
+		if(args.length > 0) {
+			rutaFichero = args[0];
+		}
+		
+		Scanner lector = new Scanner(new File(rutaFichero));
+		int contador = 0;
+		
+		while(lector.hasNext()) {
+			lector.next();//Devuelve la palabra que lee, pero no la almacenamos
+			contador++;
+		}
+		System.out.println("Total palabras: " + contador + ".");
+
+	}
+
+}
