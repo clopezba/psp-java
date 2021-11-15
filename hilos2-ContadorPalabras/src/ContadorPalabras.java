a1c3022c1409c7c18b1c403a1073752c406ff1d2
CristinaL <200101@iessierradeguara.com>
2021-11-15 10:32:41 +0100

    ..

--- hilos2-ContadorPalabras/src/ContadorPalabras.java
+++ hilos2-ContadorPalabras/src/ContadorPalabras.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class ContadorPalabras {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args){
 		//Creamos fichero 'texto.txt' -> en temrinal: echo "hola clase ¿cómo estáis?" > texto.txt
 		String rutaFichero = "/home/alumno/texto.txt";
 		//Si se le pasa un fichero por argumento, leerá ese fichero
@@ -12,14 +12,22 @@ public class ContadorPalabras {
 			rutaFichero = args[0];
 		}
 		
-		Scanner lector = new Scanner(new File(rutaFichero));
-		int contador = 0;
-		
-		while(lector.hasNext()) {
-			lector.next();//Devuelve la palabra que lee, pero no la almacenamos
-			contador++;
+		Scanner lector;
+		try {
+			lector = new Scanner(new File(rutaFichero));
+			int contador = 0;
+			
+			while(lector.hasNext()) {
+				lector.next();//Devuelve la palabra que lee, pero no la almacenamos
+				contador++;
+			}
+			System.out.println("Total palabras: " + contador + ".");
+			
+		} catch (FileNotFoundException e) {
+			System.err.println("Fichero " + rutaFichero + " no encontrado.");
+			e.printStackTrace();
 		}
-		System.out.println("Total palabras: " + contador + ".");
+		
 
 	}
 
---
+++ hilos2-ContadorPalabras/src/Ejecutor.java
@@ -0,0 +1,9 @@
+
+public class Ejecutor {
+
+	public static void main(String[] args) {
+		ContadorPalabras.main(args);
+
+	}
+
+}
