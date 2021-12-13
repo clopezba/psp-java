608b097dc40ba4b24f18c62e38d39e3387929cb7
CristinaL <200101@iessierradeguara.com>
2021-12-13 10:43:05 +0100

    ..

--- sockets01/src/sockets01/Cliente.java
+++ sockets01/src/sockets01/Cliente.java
@@ -1,13 +1,23 @@
 package sockets01;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Cliente {
 
-	public static void main(String[] args) throws UnknownHostException, IOException {
+	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
+		
+		//----SIEMPRE LANZAR ANTES DEL SERVIDOR----
+		
 		Socket socket = new Socket("localhost", 5252); 
+		DataInputStream entrada = new DataInputStream(socket.getInputStream());
+		System.out.println("El servidor me dice: " + entrada.readUTF());
+		DataOutputStream salida = new DataOutputStream(socket.getOutputStream());
+		Thread.sleep(3000);
+		salida.writeUTF("Soy el cliente!!");
 		
 	}
 
--- sockets01/src/sockets01/Servidor.java
+++ sockets01/src/sockets01/Servidor.java
@@ -1,5 +1,7 @@
 package sockets01;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,9 +9,16 @@ import java.net.Socket;
 public class Servidor {
 
 	public static void main(String[] args) throws IOException {
+		//----SIEMPRE LANZAR ANTES DEL SERVIDOR----
 		ServerSocket servSock = new ServerSocket(5252);
 		Socket socket = servSock.accept(); //Queda esperando a que alguien llame
 		System.out.println(socket.getInetAddress()); //Aparece la dirección del router, no del pc
+		
+		DataOutputStream salida = new DataOutputStream(socket.getOutputStream());
+		salida.writeUTF("Hola cliente soy el servidor");
+		DataInputStream entrada = new DataInputStream(socket.getInputStream()); //Lo que devuelve el cliente
+		System.out.println("El cliente me dice: " + entrada.readUTF());
+		
 	}
 
 }
