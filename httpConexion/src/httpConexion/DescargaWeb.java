e15fc3c39f874d46c2d7f9b25c6c12a23b95ae93
CristinaL <200101@iessierradeguara.com>
2022-01-25 12:33:42 +0100

    https

---
+++ httpConexion/src/httpConexion/DescargaWeb.java
@@ -0,0 +1,33 @@
+package httpConexion;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class DescargaWeb {
+	
+	public static void main(String[] args) throws IOException {
+		String stringURL = "https://wikipedia.orga/";
+		URL objetoURL = new URL(stringURL); //Creo URL
+		
+		//URLConnection conexionURL = objetoURL.openConnection(); //Creo conexión
+		
+		HttpsURLConnection conexionHttps = (HttpsURLConnection) objetoURL.openConnection();  
+		
+		
+		BufferedInputStream lectorURL = new BufferedInputStream(objetoURL.openStream());
+		byte[] buffer = new byte[2048]; //ponemos el tamaño que queramos
+		
+		int bytesLeidos = lectorURL.read(buffer);// -1 si no ha leido bytes
+		
+		while(bytesLeidos != -1) {
+			System.out.println(new String(buffer));
+			bytesLeidos = lectorURL.read(buffer);
+		}
+		System.out.println("El servidor devuelve: " + conexionHttps.getResponseCode());
+	}
+}
