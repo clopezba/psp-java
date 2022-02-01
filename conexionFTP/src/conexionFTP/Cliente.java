63c6c217b567079d7ec81d9a449e8798d87c084e
CristinaL <200101@iessierradeguara.com>
2022-02-01 12:40:14 +0100

    conexion FTP

---
+++ conexionFTP/src/conexionFTP/Cliente.java
@@ -0,0 +1,52 @@
+package conexionFTP;
+
+import java.io.IOException;
+import java.net.SocketException;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+
+public class Cliente {
+
+	public static void main(String[] args) throws SocketException, IOException {
+		//Crea el cliente de tipo FTP
+		FTPClient  cliente = new FTPClient();
+		
+		//Conecta con el anfitrión
+		cliente.connect("localhost");
+		
+		//Login del usuario - Devuelve un boolean
+		boolean conectado = cliente.login("uftp", "alumno");
+		if (conectado) {
+			System.out.println("¡Conexión correcta!");
+			FTPFile[] carpetas = cliente.listDirectories();
+			
+			for (FTPFile carpeta : carpetas) {
+				System.out.println(carpeta);
+				cliente.cwd(carpeta.getName());
+				FTPFile[] archivos = cliente.listFiles();
+				for (FTPFile archivo : archivos) {
+					System.out.println(archivo);
+				}
+				cliente.changeToParentDirectory();
+			}
+			
+			
+			
+			
+			
+			if(cliente.logout()) {
+				System.out.println("Conexión cerrada");
+			} else {
+				System.out.println("No se pudo cerrar la conexión");
+			}
+		}else {
+			System.out.println("¡La conexión falló!");
+		}
+		
+		
+		
+
+	}
+
+}
---
+++ conexionFTP/src/conexionFTP/SubirArchivo.java
@@ -0,0 +1,65 @@
+package conexionFTP;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.SocketException;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+
+public class SubirArchivo {
+
+	public static void main(String[] args) {
+		String servidor = "192.168.102.200";
+		int puerto = 21; // Puerto por defecto para ftp
+		String usuario = "uftp";
+		String password = "alumno";
+
+		FTPClient cliente = new FTPClient();
+
+		try {
+			cliente.connect(servidor, puerto);
+			System.out.println("Conectado");
+			if (cliente.login(usuario, password)) {
+				System.out.println("Login correcto!");
+			} else {
+				System.out.println("Login incorrecto!");
+			}
+			//FTP admite datos de texto binarios
+			cliente.setFileType(FTP.BINARY_FILE_TYPE);
+			//Abro el fichero local
+			File archivoLocal = new File("Cristina.txt");
+			//Establezco el nombre de destino
+			String archivoRemoto = "FicheroCristina.txt";
+			InputStream is = new FileInputStream(archivoLocal);
+			System.out.println("Comenzando la subida...");
+			boolean terminado = cliente.storeFile(archivoRemoto, is);
+			is.close(); //StoreFile no cierra el Stream, lo hacemos manual
+			if (terminado) {
+				System.out.println("Archivo enviado");
+			} else {
+				System.out.println("Error en la transmisión");
+			}
+
+		} catch (SocketException e) {
+			System.err.println("Error: " + e.getMessage());
+		} catch (IOException e) {
+			System.err.println("Error: " + e.getMessage());
+		} finally {
+			if (cliente.isConnected()) {
+				try {
+					cliente.logout();
+					cliente.disconnect();
+					System.out.println("Desconectado");
+				} catch (IOException e) {
+					System.err.println("Error: " + e.getMessage());
+				}
+
+			}
+		}
+
+	}
+
+}
